c7fe369e7fe309bb86d69671e8edd6486c697f3b
nothaionde <nothaionde@gmail.com>
2023-12-20 16:23:54 +0600

    added glfw and glad

---
+++ src/Engine/Core/Window.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kenes on 14.12.2023.
+//
+
+#include "Window.h"
---
+++ src/Engine/Core/Window.h
@@ -0,0 +1,15 @@
+//
+// Created by kenes on 14.12.2023.
+//
+
+#ifndef YNILA_WINDOW_H
+#define YNILA_WINDOW_H
+
+
+class Window
+{
+
+};
+
+
+#endif //YNILA_WINDOW_H
